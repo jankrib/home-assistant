63720466763eed8287596b03e29a38d8f13666b5
jankrib <jan@crondale.no>
2020-01-18 12:33:46 +0100

    Use actual xcomfort connection

--- homeassistant/components/xcomfort_bridge/light.py
+++ homeassistant/components/xcomfort_bridge/light.py
@@ -9,6 +9,8 @@ import homeassistant.helpers.config_validation as cv
 from homeassistant.components.light import ATTR_BRIGHTNESS, PLATFORM_SCHEMA, Light
 from homeassistant.const import CONF_IP_ADDRESS
 
+from xcomfort import Bridge
+
 _LOGGER = logging.getLogger(__name__)
 
 # Validation of the user's configuration
@@ -17,35 +19,49 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 )
 
 
-def setup_platform(hass, config, add_entities, discovery_info=None):
+async def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
     """Set up the Awesome Light platform."""
     # Assign configuration variables.
     # The configuration check takes care they are present.
     ip_address = config[CONF_IP_ADDRESS]
-    authKey = config["authkey"]
+    auth_key = config["authkey"]
 
     _LOGGER.info("Setup xComfort bridge: %s", ip_address)
 
-    lights = [XComfortLight("Test")]
+    bridge = await Bridge.connect(ip_address, auth_key)
 
-    # Add devices
-    add_entities(lights)
+    devices = await bridge.get_devices()
+
+    lights = []
+
+    for device_id in devices:
+        lights.append(XComfortLight(bridge, devices[device_id]))
+
+    # await bridge.close()
+
+    await async_add_devices(lights)
 
 
 class XComfortLight(Light):
     """Representation of an Awesome Light."""
 
-    def __init__(self, name):
+    def __init__(self, bridge, device):
         """Initialize an AwesomeLight."""
+        self._bridge = bridge
+        self._device = device
 
-        self._name = name
-        self._state = None
+        self._name = device.name
+        self._state = device.switch
         self._brightness = None
 
     @property
     def name(self):
         """Return the display name of this light."""
         return self._name
+
+    @property
+    def should_poll(self) -> bool:
+        return True  # TODO Change to false and call schedule_update_ha_state() when state changes
 
     @property
     def brightness(self):
@@ -61,16 +77,18 @@ class XComfortLight(Light):
         """Return true if light is on."""
         return self._state
 
-    def turn_on(self, **kwargs):
+    async def async_turn_on(self, **kwargs):
         """Instruct the light to turn on.
 
         You can skip the brightness part if your light does not support
         brightness control.
         """
+        await self._bridge.switch_device(self._device.device_id, True)
         self._state = True
 
-    def turn_off(self, **kwargs):
+    async def async_turn_off(self, **kwargs):
         """Instruct the light to turn off."""
+        await self._bridge.switch_device(self._device.device_id, False)
         self._state = False
 
     def update(self):
